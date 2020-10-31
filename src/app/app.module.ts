9f1f6f09432ffc8def0930dc2f98668b27e8592e
JY Liu <ljieyao0210@gmail.com>
2020-10-31 23:08:27 +0800

    import WebBluetoothModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,20 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { WebBluetoothModule } from '@manekinekko/angular-web-bluetooth';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+
+    WebBluetoothModule.forRoot({
+      enableTracing: true // or false, this will enable logs in the browser's console
+    })
+  ],
   providers: [
     StatusBar,
     SplashScreen,
